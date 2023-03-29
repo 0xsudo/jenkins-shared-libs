768215903e05cc8d45c6c7f5ec8109c0e5a1bb61
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-29 13:20:05 +0300

    parsed image name as variable

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,10 +1,10 @@
-def call() {
+def call(String imagename) {
     retry(count: 3) {
 		withDockerRegistry([credentialsId: 'docker-login', url: '']) {
 			script {
 				if (params.ecr_action == 'create') {
 					// docker.build('buggy-app')
-					sh 'docker build -t buggy-app .'
+					sh 'docker build -t ${imagename} .'
 				}
 			}
 		}
