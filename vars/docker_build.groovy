d07a07b7d6b53cecfb18dd2096a0a1d1a05902a8
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-30 13:04:09 +0300

    imported utility class and parsed our param

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,10 +1,14 @@
-def call() {
+import devops.jnkns.Utils
+
+def call(Map params = [:]) {
+	params = Utils.parseParams(this, params)
+
     retry(count: 3) {
 		withDockerRegistry([credentialsId: 'docker-login', url: '']) {
 			script {
 				if (params.ecr_action == 'create') {
 					// docker.build('buggy-app')
-					sh 'docker build -t buggy-app .'
+					sh 'docker build -t ${params.imagename} .'
 				}
 			}
 		}
