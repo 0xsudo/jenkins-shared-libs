5c5c20827d898764e051f718d1efbe6177107b1e
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-30 19:03:09 +0300

    defining param inside script block params

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,14 @@
 // import devops.jnkns.ParamParser
 
-def call(Map params) {
+def call(Map config) {
     // Access the parameters
-    String imagename = params.imagename
-    // String param2 = params.param2
+    // String imagename = config.imagename
+    // String param2 = config.param2
 
 	withDockerRegistry([credentialsId: 'docker-login', url: '']) {
 		script {
+			String imagename = config.imagename
+			
 			if (params.ecr_action == 'create') {
 				// docker.build('buggy-app')
 				sh 'docker build -t ${imagename} .'
