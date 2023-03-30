b78323cc15fde6fa61c969248360469d5d49a5bd
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-30 12:58:43 +0300

    added a utility class

---
+++ src/devops/jnkns/Utils.groovy
@@ -0,0 +1,10 @@
+package devops.jnkns
+
+class Utils{
+    static Map parseParams(def context, Map params = [:]) {
+        context.request.parameterNames.each { paramName ->
+            params[paramName] = context.request.getParameter(paramName)
+        }
+        return params
+    }
+}
